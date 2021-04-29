443474b131f17d24f25427c12e3c616b9fbeb6c9
Vladislav Bezruk <bezrukvladislav@gmail.com>
2021-04-29 20:32:26 +0300

    Changed

--- main.cpp
+++ main.cpp
@@ -1,50 +1,276 @@
-#include "include\graphics.h"
+#include <graphics.h>
 #include <stdio.h>
 #include <conio.h>
-#include "include\functions.h"
-#include "include\graph.h"
 #include <math.h>
-#include "include\structures.cpp"
 
-#define INPUT      "files/input.txt"
-#define OUTPUT     "files/output.txt"
-#define PI         3.141592653
-#define HEIGHT     600
-#define WIDTH      800
-#define DATA_COUNT 3
+#define INPUT           "files/input.txt"
+#define OUTPUT          "files/output.txt"
+#define PI              3.141592653
+#define HEIGHT          720
+#define WIDTH           1280
+#define STR_LEN         99
+#define DATA_COUNT      3
+#define EROR_SQRT       "It is impossible to calculate the square root"
+#define ERROR_NOFILE    "There are no input file"
+#define ERROR_EMPTYFILE "Input file is empty"
 
-int main() {
-	initwindow(WIDTH, HEIGHT);
-	 
-	/*                    
-	moveto(0, 0);         
-	lineto(400, 300);       
-	getch();
-	cleardevice();
-	getch();            
-	            
-	closegraph(); 
-	*/
-	int n = 21;
-	
-	double x[n] = {0, 30, 60, 90, 120, 150, 180, 210, 240, 270, 300, 330, 360, 390, 420, 450, 480, 510, 540, 570, 600};
-	double y[n] = {0.000000, 0.000004, 0.000008, 0.000013, 0.000018, 0.000023, 0.000027, 0.000031, 0.000035, 0.000039, 0.000042, 0.000046, 0.000049, 0.000052, 0.000056, 0.000059, 0.000059, 0.000058, 0.000058, 0.000057, 0.000057};
-	
-	
-	draw(x, y, n, "T, c", "I, À");
-	
-	getch();
-	
+struct DATA {
+	double T;
+	double dt;
+	double L0;
+	int k0;
+	double C0;
+	int m;
+	int n;
+};
+
+struct DATA_RES {
+	int n, num;
+	double *x, *y;
+	char *xLabel, *yLabel, *xSystem, *ySystem;
+};
+
+double getMin(double arr[], int n);
+
+double getMax(double arr[], int n);
+
+int convert(double c, double dc, double cMin);
+
+bool isBetween(double x, double a, double b);
+
+double calcC(double t, DATA data);
+
+double calcK(double t, DATA data);
+
+double calcL(double t, double k, DATA data);
+
+DATA readData(FILE *input);
+
+double calcT(double L, double C);
+
+void draw(DATA_RES result);
+
+double calcT(double t, DATA data);
+
+DATA_RES tabulate(DATA data, int num, char* xLabel, char* yLabel, char* xSystem, char* ySystem);
+
+void printResult(DATA_RES result, FILE *output);
+
+int main() {	
+
 	DATA data[DATA_COUNT];
+	DATA_RES res[DATA_COUNT];
+	FILE *input, *output;
+	int i;
 	
-	FILE *input;
-	
+	initwindow(WIDTH, HEIGHT);
+
 	input = fopen(INPUT, "r");
+	output = fopen(OUTPUT, "w");
 	
-	for (int i = 0; i < DATA_COUNT; i++) {
-		data[i] = readData(input);
-		printf("%lf %lf %lf %d %lg %d %d\n", data[i].T, data[i].dt, data[i].L0, data[i].k0, data[i].C0, data[i].m, data[i].n);
+	if (input == 0) {
+		puts(ERROR_NOFILE);
+		return 0;
 	}
 	
+	fseek(input, 0, SEEK_END);
+    if (ftell(input) == 0) { 
+        puts(ERROR_EMPTYFILE);  
+        return 0;
+    } 
+    fseek(input, 0, SEEK_SET);
+	
+	for (i = 0; i < DATA_COUNT; i++) {
+		data[i] = readData(input);
+			
+		res[i] = tabulate(data[i], i + 1, "t", "Tk", "c.", "c.");
+		
+		draw(res[i]);
+		
+		printResult(res[i], output);
+		
+		getch();
+	}
+	
+	fclose(input);
+	fclose(output);
+	
 	return 0;
+}
+
+double getMin(double arr[], int n) {
+	int i, iMin = 0;
+	
+	for (i = 1; i < n; i++) {
+		if (arr[iMin] > arr[i]) {
+			iMin = i;
+		}
+	}
+	
+	return arr[iMin];	
+}
+
+double getMax(double arr[], int n) {
+	int i, iMax = 0;
+	
+	for (i = 1; i < n; i++) {
+		if (arr[iMax] < arr[i]) {
+			iMax = i;
+		}
+	}
+	
+	return arr[iMax];	
+}
+
+int convert(double c, double dc, double cMin) {
+	return round((c - cMin) / dc);	
+}
+
+bool isBetween(double x, double a, double b) {
+	if (x >= a && x <= b) {
+		return true;
+	}
+	return false;
+}
+
+double calcC(double t, DATA data) {
+	if (isBetween(t, 0, data.T / 3)) {
+		return data.C0 * (1 - exp(-1 * data.n * t));
+	}
+	if (isBetween(t, data.T / 3, data.T)) {
+		return data.C0 * (1 - exp(-1 * data.n * data.T / 3));
+	}	
+}
+
+double calcK(double t, DATA data) {
+	if (isBetween(t, 0, data.T / 4)) {
+		return data.k0 * (1 + exp(-1 * data.m * t));
+	}
+	if (isBetween(t, data.T / 4, data.T)) {
+		return data.k0 * (1 + exp(-1 * data.m * data.T / 4));
+	}		
+}
+
+double calcL(double t, double k, DATA data) {
+	if (isBetween(t, 0, data.T / 2)) {
+		return data.L0 * (1 - exp(-1 * k * t));
+	}
+	if (isBetween(t, data.T / 2, data.T)) {
+		return data.L0 * (1 - exp(-1 * k * data.T / 2));
+	}
+}
+
+DATA readData(FILE *input) {
+	DATA data;
+	
+	fscanf(input, "%lf%lf%lf%d%lf%d%d", 
+	       &data.T, &data.dt, &data.L0, &data.k0,
+		   &data.C0, &data.m, &data.n);
+		   
+	return data;
+}
+
+void draw(DATA_RES result) {
+	char xLabelG[STR_LEN];
+	char yLabelG[STR_LEN];
+	
+	double xMin = getMin(result.x, result.n);
+	double xMax = getMax(result.x, result.n);
+	
+	double yMin = getMin(result.y, result.n);
+	double yMax = getMax(result.y, result.n);
+	
+	int w = getmaxx();
+	int h = getmaxy();
+	
+	double dx = (xMax - xMin) / w;
+	double dy = (yMax - yMin) / h;
+	
+	char label[STR_LEN] = "Result ";
+	char strNum[3] = {result.num + '0', ':', '\0'};
+	
+	int i;
+	
+	cleardevice();
+	
+	for (i = 0; i < result.n - 1; i++) {
+		line(convert(result.x[i], dx, xMin), 
+		h - convert(result.y[i], dy, yMin), 
+		convert(result.x[i + 1], dx, xMin), 
+		h - convert(result.y[i + 1], dy, yMin));
+	}
+	
+	strcat(label, strNum);
+	
+	outtextxy(w / 2 - 10 * strlen(label), 0, label);
+	
+	strcpy(xLabelG, result.xLabel);
+	strcpy(yLabelG, result.yLabel);
+	
+	strcat(yLabelG, ", ");
+	strcat(yLabelG, result.ySystem);
+	
+	strcat(xLabelG, ", ");
+	strcat(xLabelG, result.xSystem);
+	
+	outtextxy(0, 0, yLabelG);
+	outtextxy(w - 10 * strlen(xLabelG), h - 20, xLabelG);
+}
+
+double calcT(double t, DATA data) {
+	return calcT(calcL(t, calcK(t, data), data), calcC(t, data));
+}
+
+double calcT(double L, double C) {
+	if (L * C >= 0) {
+		return 2 * PI * sqrt(L * C);
+	} else {
+		puts(EROR_SQRT);
+		getch();
+	}
+}
+
+DATA_RES tabulate(DATA data, int num, char* xLabel, char* yLabel, char* xSystem, char* ySystem) {
+	DATA_RES result;
+	int i = 0;
+	double t = 0;
+
+	result.n = (data.T / data.dt) + 1;
+	result.num = num;
+	result.xLabel = xLabel;
+	result.yLabel = yLabel;
+	result.xSystem = xSystem;
+	result.ySystem = ySystem;
+	
+	result.x = (double*)calloc(result.n, sizeof(double)); 
+	result.y = (double*)calloc(result.n, sizeof(double)); 
+		
+	do {
+		result.x[i] = t;
+		result.y[i] = calcT(t, data);
+		
+		t += data.dt;
+		i++;
+	} while (t <= data.T);
+	
+	result.x[result.n - 1] = data.T;
+	result.y[result.n - 1] = calcT(data.T, data);
+	
+	return result;
+}
+
+void printResult(DATA_RES result, FILE *output) {
+	int i;
+	printf("Result %d:\n", result.num);
+	fprintf(output, "Result %d:\n", result.num);
+	
+	for (i = 0; i < result.n; i++) {
+		printf("%s = %lf %s\t %s = %.18lf %s\n", result.xLabel, result.x[i],
+		                                         result.xSystem, result.yLabel,
+											     result.y[i], result.ySystem);
+											   
+		fprintf(output, "%s = %lf %s\t %s = %.18lf %s\n", result.xLabel, result.x[i],
+		                                                  result.xSystem, result.yLabel,
+											              result.y[i], result.ySystem);
+	}	
 }
